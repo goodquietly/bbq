50620881f870afeadd22785244b205b3536c5899
Aleksei Iurin <19alex85@gmail.com>
2022-06-13 20:00:31 +0300

    validations fix

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -11,10 +11,8 @@ class Subscription < ApplicationRecord
   validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
   validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
 
-  validates :user, comparison: { other_than: -> { event.user } }, if: -> { user.present? }
-
-  validates :user_email, exclusion: { in: User.pluck(:email), message: 'Почта %{value} уже занята.' },
-                         unless: -> { user.present? }
+  validate :check_who_is_the_owner, if: -> { user.present? }
+  validate :check_if_email_is_busy, unless: -> { user.present? }
 
   def user_name
     if user.present?
@@ -30,5 +28,15 @@ class Subscription < ApplicationRecord
     else
       super
     end
+  end
+
+  private
+
+  def check_who_is_the_owner
+    errors.add(:user, I18n.t('activerecord.models.errors.event_owner')) if user == event.user
+  end
+
+  def check_if_email_is_busy
+    errors.add(:user_email, I18n.t('activerecord.models.errors.email_is_busy')) if User.exists?(email: user_email)
   end
 end
