a419c3ddbecf884509d61e8a78d414cd0773debb
Aleksei Iurin <19alex85@gmail.com>
2022-06-23 23:34:02 +0300

    some form changes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ApplicationRecord
   belongs_to :user
 
   has_one_attached :photo do |attachable|
-    attachable.variant :thumb, resize_to_limit: [400, 1000]
+    attachable.variant :thumb, resize_to_limit: [200, 200]
   end
 
   validates :photo, file_size: { less_than_or_equal_to: 4.megabytes, message: I18n.t('activerecord.models.file_size') },
