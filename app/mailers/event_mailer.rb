5bb1f8713180c7b9a5e19d1bde166f33878be101
Aleksei Iurin <19alex85@gmail.com>
2022-06-23 09:31:18 +0300

    email subjects localization

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,19 +4,19 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: t('mailer.new_subscription', event_title: event.title)
+    mail(to: event.user.email, subject: default_i18n_subject(event_title: event.title))
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: t('mailer.new_comment', event_title: event.title)
+    mail(to: email, subject: default_i18n_subject(event_title: event.title))
   end
 
   def photo(photo, email)
     @photo = photo
 
-    mail to: email, subject: t('mailer.new_photo', event_title: photo.event.title)
+    mail(to: email, subject: default_i18n_subject(event_title: photo.event.title))
   end
 end
