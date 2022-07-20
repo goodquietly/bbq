33cd3699500fb08c7b30f25ba817ee1621788c31
Aleksei Iurin <19alex85@gmail.com>
2022-07-21 00:20:34 +0300

    all today fixs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :mailjet_api
-  config.action_mailer.default_url_options = { host: 'bbqgp.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'bbqgp.site' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
