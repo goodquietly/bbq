221514a84d289422271195e9e2714cc36f764e7a
Aleksei Iurin <19alex85@gmail.com>
2022-11-04 20:48:39 +0300

    user delete bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
-    devise_for :users, skip: :omniauth_callbacks
+    devise_for :users, skip: :omniauth_callbacks, controllers: { registrations: 'users/registrations' }
 
     resources :users, only: %i[show edit update]
 
